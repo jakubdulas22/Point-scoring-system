bc93da2c74bf64ff30a7e02e0a977d77f2dd11ac
Jakub Dulas <j-dulas@student.newbury-college.ac.uk>
2021-03-30 11:39:47 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Point_scoring_system
+{
+    public partial class Points :Form
+    {
+        public Points()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //here we could have the team/ invdividuals appear when the arrow is hit
+            
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //how many points
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            this.Close();
+            // this closes the whole software 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
