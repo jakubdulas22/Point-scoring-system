f4b349e08e181357fee437cd7dc58f285f129265
Jakub Dulas <j-dulas@student.newbury-college.ac.uk>
2021-03-31 10:10:29 +0100

    Added the exit and save buttons (save does not work) also added 2 textboxes

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Point_scoring_system
 {
-    public partial class Points :Form
+    public partial class Points : Form
     {
         public Points()
         {
@@ -20,12 +13,12 @@ namespace Point_scoring_system
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             //here we could have the team/ invdividuals appear when the arrow is hit
-            
+
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             //how many points
-          
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -41,7 +34,7 @@ namespace Point_scoring_system
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
